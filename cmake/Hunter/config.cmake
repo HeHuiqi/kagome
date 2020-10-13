ab882740536be22f2e50ebb66f712969cc45cfc6
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2020-10-13 20:42:37 +0300

    SHOULD BE REVERTED: changes to build libp2p as part of kagome
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -5,11 +5,11 @@
 #    CMAKE_ARGS "CMAKE_VARIABLE=value"
 #)
 
-hunter_config(libp2p
-    URL https://github.com/xDimon/cpp-libp2p/archive/56aecfaec13fdb11155a998177032e9e98039956.zip
-    SHA1 e578970381abcc920ca40086702fc0921cf80896
-    CMAKE_ARGS TESTING=OFF
-    )
+#hunter_config(libp2p
+#    URL https://github.com/xDimon/cpp-libp2p/archive/56aecfaec13fdb11155a998177032e9e98039956.zip
+#    SHA1 e578970381abcc920ca40086702fc0921cf80896
+#    CMAKE_ARGS TESTING=OFF
+#    )
 
 # TODO (yuraz): PRE-451 remove it after collecting libsecp256k1 into hunter config
 hunter_config(libsecp256k1
